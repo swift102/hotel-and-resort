79a9907c994037ca9427d9ab175325b3480e5d58
Vincent Chitsike <122750383+swift102@users.noreply.github.com>
2025-01-14 02:11:03 +0200

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { BookingComponent } from './booking/booking.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    BookingComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
