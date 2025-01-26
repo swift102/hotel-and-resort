46d8d278e6660595fad71daf50c51ff9d1d4108d
Vincent Chitsike <122750383+swift102@users.noreply.github.com>
2025-01-26 19:35:05 +0200

    Room Updates

--- hotel-and-resort  --- Frontend/src/app/app-routing.module.ts
+++ hotel-and-resort  --- Frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
+  {path: 'rooms', component: RoomComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
